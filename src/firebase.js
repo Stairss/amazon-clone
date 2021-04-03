3cde340c0c7306808c268770f1d6e70d0bc9feee
Dawid Spisak <dawidspisak@gmail.com>
2021-04-03 19:58:46 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDueZxo2G9z3EOBHxUxZJE3zuX5f4cLZWc",
+    authDomain: "clone-ba7cf.firebaseapp.com",
+    projectId: "clone-ba7cf",
+    storageBucket: "clone-ba7cf.appspot.com",
+    messagingSenderId: "280619628129",
+    appId: "1:280619628129:web:0df730de373b283cc4dc20",
+    measurementId: "G-ENFYX9LR2R"
+};
